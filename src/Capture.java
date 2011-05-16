4dcf2f05077b43274784b55b5b7ad951085ae997
Josh Cough <joshcough@gmail.com>
2011-05-16 14:11:47 -0500

    formatted

--- src/Capture.java
+++ src/Capture.java
@@ -1,270 +1,229 @@
-package org.nlogo.extensions.qtj ;
+package org.nlogo.extensions.qtj;
 
 import org.nlogo.api.DefaultClassManager;
 import org.nlogo.api.PrimitiveManager;
-import org.nlogo.api.Syntax ;
-import org.nlogo.api.Context ;
+import org.nlogo.api.Syntax;
+import org.nlogo.api.Context;
 import org.nlogo.api.DefaultReporter;
 import org.nlogo.api.DefaultCommand;
 import org.nlogo.api.Argument;
-import org.nlogo.api.ExtensionException ;
-import org.nlogo.api.LogoException ;
+import org.nlogo.api.ExtensionException;
+import org.nlogo.api.LogoException;
 
-import quicktime.std.sg.SequenceGrabber ;
-import quicktime.QTSession ;
-import quicktime.std.sg.SGVideoChannel ;
-import quicktime.qd.* ;
-import quicktime.std.sg.SGDeviceList ;
-import quicktime.util.RawEncodedImage ;
-import quicktime.io.QTFile ;
-import quicktime.util.RawEncodedImage ;
+import quicktime.std.sg.SequenceGrabber;
+import quicktime.QTSession;
+import quicktime.std.sg.SGVideoChannel;
+import quicktime.qd.*;
+import quicktime.std.sg.SGDeviceList;
+import quicktime.util.RawEncodedImage;
+import quicktime.io.QTFile;
+import quicktime.util.RawEncodedImage;
 
-public strictfp class Capture
-{
-	private static SequenceGrabber capture ;
-	private static QDGraphics graphics ;
-	private static float height ;
+public strictfp class Capture {
+  private static SequenceGrabber capture;
+  private static QDGraphics graphics;
+  private static float height;
 
-	public static void unload() 
-		throws ExtensionException
-	{
-		try
-		{
-			if( capture != null )
-			{
-				capture.stop() ;
-				capture = null ;
-				graphics = null ;
-				QTSession.close() ;
-			}
-		}
-		catch( quicktime.std.StdQTException e )
-		{
-			throw new ExtensionException( e.getMessage() ) ;
-		}
-	}
+  public static void unload()
+      throws ExtensionException {
+    try {
+      if (capture != null) {
+        capture.stop();
+        capture = null;
+        graphics = null;
+        QTSession.close();
+      }
+    } catch (quicktime.std.StdQTException e) {
+      throw new ExtensionException(e.getMessage());
+    }
+  }
 
-	public static class StartCamera
-		extends DefaultCommand
-	{
-		public Syntax getSyntax()
-		{
-			return Syntax.commandSyntax( new int [] { Syntax.TYPE_NUMBER , Syntax.TYPE_NUMBER } ) ;			
-		}
-		public String getAgentClassString()
-		{
-			return "O" ;
-		}
-		public void perform( Argument args[] , Context context )
-			throws ExtensionException , LogoException
-		{
-			double patchSize = context.getAgent().world().patchSize() ;
-			float width = (float) ( args[ 0 ].getDoubleValue() * patchSize ) ;
-			float height = (float) ( args[ 1 ].getDoubleValue() * patchSize ) ;
-			
-			try
-			{	
-				QDRect rect = new QDRect( width , height ) ;
-				SGVideoChannel channel = getVideoChannel( rect ) ;
-				initializeChannel( channel , rect ) ;
-			}
-			catch( quicktime.std.StdQTException e )
-			{
-				String msg = "Failed to open a session.  QuickTime may not be installed properly. Or your camera may not be connected or on." ;
-				if( System.getProperty( "os.name" ).startsWith( "Windows" ) )
-				{
-					msg += " Perhaps WinVDig is not installed." ;
-				}
-				throw new ExtensionException( msg ) ;
-			}
-			catch( quicktime.QTException e )
-			{
-				throw new ExtensionException( e.getMessage() ) ;
-			}
-		}				
-	}
+  public static class StartCamera
+      extends DefaultCommand {
+    public Syntax getSyntax() {
+      return Syntax.commandSyntax(new int[]{Syntax.TYPE_NUMBER, Syntax.TYPE_NUMBER});
+    }
 
-	private static SGVideoChannel getVideoChannel( QDRect rect )
-		throws quicktime.QTException , ExtensionException
-	{
-		unload() ;
-		
-		QTSession.open() ;
+    public String getAgentClassString() {
+      return "O";
+    }
 
-		// workaround for intel macs (from imagej)
-		graphics = quicktime.util.EndianOrder.isNativeLittleEndian() 
-			? new QDGraphics( QDConstants.k32BGRAPixelFormat , rect ) 
-			: new QDGraphics( QDGraphics.kDefaultPixelFormat , rect ) ;
+    public void perform(Argument args[], Context context)
+        throws ExtensionException, LogoException {
+      double patchSize = context.getAgent().world().patchSize();
+      float width = (float) (args[0].getDoubleValue() * patchSize);
+      float height = (float) (args[1].getDoubleValue() * patchSize);
 
-		capture = new SequenceGrabber() ;
-		capture.setGWorld( graphics , null ) ;
-		
-		return new SGVideoChannel( capture ) ;
-	}
+      try {
+        QDRect rect = new QDRect(width, height);
+        SGVideoChannel channel = getVideoChannel(rect);
+        initializeChannel(channel, rect);
+      } catch (quicktime.std.StdQTException e) {
+        String msg = "Failed to open a session.  QuickTime may not be installed properly. Or your camera may not be connected or on.";
+        if (System.getProperty("os.name").startsWith("Windows")) {
+          msg += " Perhaps WinVDig is not installed.";
+        }
+        throw new ExtensionException(msg);
+      } catch (quicktime.QTException e) {
+        throw new ExtensionException(e.getMessage());
+      }
+    }
+  }
 
-	private static void initializeChannel( SGVideoChannel channel , QDRect rect )
-		throws quicktime.QTException
-	{
-		channel.setBounds( rect ) ;
-		channel.setUsage( quicktime.std.StdQTConstants.seqGrabRecord |
-						  quicktime.std.StdQTConstants.seqGrabPreview |
-						  quicktime.std.StdQTConstants.seqGrabPlayDuringRecord ) ;
+  private static SGVideoChannel getVideoChannel(QDRect rect)
+      throws quicktime.QTException, ExtensionException {
+    unload();
 
-		channel.setFrameRate( 0 ) ;
-		channel.setCompressorType( quicktime.std.StdQTConstants.kComponentVideoCodecType ) ;
+    QTSession.open();
 
-		QTFile movieFile = new QTFile( new java.io.File( "NoFile" ) ) ;
-		capture.setDataOutput( null , quicktime.std.StdQTConstants.seqGrabDontMakeMovie ) ;
-		capture.prepare( true , true ) ;
-		capture.startRecord() ;
-		capture.idle() ;
-		capture.update( null ) ;
-	}
+    // workaround for intel macs (from imagej)
+    graphics = quicktime.util.EndianOrder.isNativeLittleEndian()
+        ? new QDGraphics(QDConstants.k32BGRAPixelFormat, rect)
+        : new QDGraphics(QDGraphics.kDefaultPixelFormat, rect);
 
-	public static class SelectCamera
-		extends DefaultCommand
-	{
-		public Syntax getSyntax()
-		{
-			return Syntax.commandSyntax( new int [] { Syntax.TYPE_NUMBER , Syntax.TYPE_NUMBER } ) ;			
-		}
-		public String getAgentClassString()
-		{
-			return "O" ;
-		}
-		public void perform( Argument args[] , Context context )
-			throws ExtensionException , LogoException
-		{
-			double patchSize = context.getAgent().world().patchSize() ;
-			float width = (float) ( args[ 0 ].getDoubleValue() * patchSize ) ;
-			float height = (float) ( args[ 1 ].getDoubleValue() * patchSize ) ;
-			
-			try
-			{	
-				QDRect rect = new QDRect( width , height ) ;
-				SGVideoChannel channel = getVideoChannel( rect ) ;
-				quicktime.std.sg.SGDeviceList list = channel.getDeviceList( 0 ) ;
-				String[] options = new String[ list.getCount() ] ;
-				int numDevices = 0 ;
-				for( int i = 0 ; i < list.getCount() ; i ++ )
-				{
-					quicktime.std.sg.SGDeviceName device = list.getDeviceName( i ) ;
-					if ( ( device.getFlags() & quicktime.std.StdQTConstants.sgDeviceNameFlagDeviceUnavailable ) == 0 )
-					{
-						options[ numDevices ] = device.getName() ;
-						numDevices ++ ;
-					}
-				}
+    capture = new SequenceGrabber();
+    capture.setGWorld(graphics, null);
 
-				if( numDevices == 0 )
-				{
-					throw new ExtensionException( "There are no cameras connected to your computer" ) ;
-				}
+    return new SGVideoChannel(capture);
+  }
 
-				if( numDevices != options.length )
-				{
-					String [] tmp = new String[ numDevices ] ;
-					System.arraycopy( options , 0 , tmp , 0 , numDevices ) ;
-					options = tmp ;
-				}				
+  private static void initializeChannel(SGVideoChannel channel, QDRect rect)
+      throws quicktime.QTException {
+    channel.setBounds(rect);
+    channel.setUsage(quicktime.std.StdQTConstants.seqGrabRecord |
+        quicktime.std.StdQTConstants.seqGrabPreview |
+        quicktime.std.StdQTConstants.seqGrabPlayDuringRecord);
 
-				Object selected = javax.swing.JOptionPane.showInputDialog
-					( null , "Select an input device: " , "QTJ Extension" ,
-					  javax.swing.JOptionPane.QUESTION_MESSAGE ,
-					  null , options , options[ 0 ] ) ;
-				if( selected != null )
-				{
-					channel.setDevice( selected.toString() ) ;
-					initializeChannel( channel , rect ) ;
-				}
-			}
-			catch( quicktime.std.StdQTException e )
-			{
-				String msg = "Failed to open a session.  QuickTime may not be installed properly." ;
-				if( System.getProperty( "os.name" ).startsWith( "Windows" ) )
-				{
-					msg += " Perhaps WinVDig is not installed." ;
-				}
-				throw new ExtensionException( msg ) ;
-			}
-			catch( quicktime.QTException e )
-			{
-				throw new ExtensionException( "Your QuickTime installation is not configured properly" ) ;
-			}
-		}				
-	}
+    channel.setFrameRate(0);
+    channel.setCompressorType(quicktime.std.StdQTConstants.kComponentVideoCodecType);
 
-	public static class StopCamera
-		extends DefaultCommand
-	{
-		public Syntax getSyntax()
-		{
-			return Syntax.commandSyntax( new int [] {} ) ;			
-		}
-		public String getAgentClassString()
-		{
-			return "O" ;
-		}
-		public void perform( Argument args[] , Context context )
-			throws ExtensionException , LogoException
-		{
-			if( capture == null )
-			{
-				throw new ExtensionException( "There is no open camera session" ) ;
-			}
-			try
-			{
-				capture.stop() ;
-				capture = null ;
-				graphics = null ;
-				QTSession.close() ;
-			}
-			catch( Exception e )
-			{
-				e.printStackTrace() ;
-				throw new ExtensionException( e.getMessage() ) ;
-			}
-		}
-	}
+    QTFile movieFile = new QTFile(new java.io.File("NoFile"));
+    capture.setDataOutput(null, quicktime.std.StdQTConstants.seqGrabDontMakeMovie);
+    capture.prepare(true, true);
+    capture.startRecord();
+    capture.idle();
+    capture.update(null);
+  }
 
-	public static class Image
-		extends DefaultReporter
-	{
-		public Syntax getSyntax()
-		{
-			return Syntax.reporterSyntax
-				( new int[] {} , Syntax.TYPE_WILDCARD ) ;
-		}
-		public String getAgentClassString()
-		{
-			return "O" ;
-		}
-		public Object report( Argument args[] , Context context )
-			throws ExtensionException , LogoException
-		{			
-			if( capture == null )
-			{
-				throw new ExtensionException( "There is no open camera session" ) ;
-			}
-			try
-			{
-				capture.idle() ;
-				capture.update( null ) ;
-				PixMap map = graphics.getPixMap() ;
-				RawEncodedImage image = map.getPixelData() ;
-				int intsPerRow = image.getRowBytes() / 4 ;
-				int height = graphics.getBounds().getHeight() ;
-				
-				int [] data = new int[ intsPerRow * height ] ;
-				image.copyToArray( 0 , data , 0 , data.length ) ;
+  public static class SelectCamera
+      extends DefaultCommand {
+    public Syntax getSyntax() {
+      return Syntax.commandSyntax(new int[]{Syntax.TYPE_NUMBER, Syntax.TYPE_NUMBER});
+    }
 
-				return QTJExtension.getBufferedImage( data , intsPerRow , height ) ;
-			}
-			catch( Exception e )
-			{
-				e.printStackTrace() ;
-				throw new ExtensionException( e.getMessage() ) ;
-			}
-		}
-	}
+    public String getAgentClassString() {
+      return "O";
+    }
+
+    public void perform(Argument args[], Context context)
+        throws ExtensionException, LogoException {
+      double patchSize = context.getAgent().world().patchSize();
+      float width = (float) (args[0].getDoubleValue() * patchSize);
+      float height = (float) (args[1].getDoubleValue() * patchSize);
+
+      try {
+        QDRect rect = new QDRect(width, height);
+        SGVideoChannel channel = getVideoChannel(rect);
+        quicktime.std.sg.SGDeviceList list = channel.getDeviceList(0);
+        String[] options = new String[list.getCount()];
+        int numDevices = 0;
+        for (int i = 0; i < list.getCount(); i++) {
+          quicktime.std.sg.SGDeviceName device = list.getDeviceName(i);
+          if ((device.getFlags() & quicktime.std.StdQTConstants.sgDeviceNameFlagDeviceUnavailable) == 0) {
+            options[numDevices] = device.getName();
+            numDevices++;
+          }
+        }
+
+        if (numDevices == 0) {
+          throw new ExtensionException("There are no cameras connected to your computer");
+        }
+
+        if (numDevices != options.length) {
+          String[] tmp = new String[numDevices];
+          System.arraycopy(options, 0, tmp, 0, numDevices);
+          options = tmp;
+        }
+
+        Object selected = javax.swing.JOptionPane.showInputDialog
+            (null, "Select an input device: ", "QTJ Extension",
+                javax.swing.JOptionPane.QUESTION_MESSAGE,
+                null, options, options[0]);
+        if (selected != null) {
+          channel.setDevice(selected.toString());
+          initializeChannel(channel, rect);
+        }
+      } catch (quicktime.std.StdQTException e) {
+        String msg = "Failed to open a session.  QuickTime may not be installed properly.";
+        if (System.getProperty("os.name").startsWith("Windows")) {
+          msg += " Perhaps WinVDig is not installed.";
+        }
+        throw new ExtensionException(msg);
+      } catch (quicktime.QTException e) {
+        throw new ExtensionException("Your QuickTime installation is not configured properly");
+      }
+    }
+  }
+
+  public static class StopCamera
+      extends DefaultCommand {
+    public Syntax getSyntax() {
+      return Syntax.commandSyntax(new int[]{});
+    }
+
+    public String getAgentClassString() {
+      return "O";
+    }
+
+    public void perform(Argument args[], Context context)
+        throws ExtensionException, LogoException {
+      if (capture == null) {
+        throw new ExtensionException("There is no open camera session");
+      }
+      try {
+        capture.stop();
+        capture = null;
+        graphics = null;
+        QTSession.close();
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw new ExtensionException(e.getMessage());
+      }
+    }
+  }
+
+  public static class Image
+      extends DefaultReporter {
+    public Syntax getSyntax() {
+      return Syntax.reporterSyntax
+          (new int[]{}, Syntax.TYPE_WILDCARD);
+    }
+
+    public String getAgentClassString() {
+      return "O";
+    }
+
+    public Object report(Argument args[], Context context)
+        throws ExtensionException, LogoException {
+      if (capture == null) {
+        throw new ExtensionException("There is no open camera session");
+      }
+      try {
+        capture.idle();
+        capture.update(null);
+        PixMap map = graphics.getPixMap();
+        RawEncodedImage image = map.getPixelData();
+        int intsPerRow = image.getRowBytes() / 4;
+        int height = graphics.getBounds().getHeight();
+
+        int[] data = new int[intsPerRow * height];
+        image.copyToArray(0, data, 0, data.length);
+
+        return QTJExtension.getBufferedImage(data, intsPerRow, height);
+      } catch (Exception e) {
+        e.printStackTrace();
+        throw new ExtensionException(e.getMessage());
+      }
+    }
+  }
 }
--- src/QTJExtension.java
+++ src/QTJExtension.java
@@ -1,111 +1,100 @@
-package org.nlogo.extensions.qtj ;
+package org.nlogo.extensions.qtj;
 
 import org.nlogo.api.DefaultClassManager;
 import org.nlogo.api.PrimitiveManager;
-import org.nlogo.api.Syntax ;
-import org.nlogo.api.Context ;
+import org.nlogo.api.Syntax;
+import org.nlogo.api.Context;
 import org.nlogo.api.DefaultReporter;
 import org.nlogo.api.DefaultCommand;
 import org.nlogo.api.Argument;
-import org.nlogo.api.ExtensionException ;
-import org.nlogo.api.ExtensionManager ;
-import org.nlogo.api.LogoException ;
+import org.nlogo.api.ExtensionException;
+import org.nlogo.api.ExtensionManager;
+import org.nlogo.api.LogoException;
 
-import java.awt.image.* ;
+import java.awt.image.*;
 
 public strictfp class QTJExtension
-	extends DefaultClassManager
-{
-	public void runOnce( org.nlogo.api.ExtensionManager em ) 
-		throws ExtensionException
-	{
-		// first check that we can find the quicktime jar 
-		// if we can't it throws an extension exception ev 3/3/09
-		em.getFile( "qtj/QTJava.jar" ) ;
-		em.addToLibraryPath( this , "lib" ) ;
-	}
+    extends DefaultClassManager {
+  public void runOnce(org.nlogo.api.ExtensionManager em)
+      throws ExtensionException {
+    // first check that we can find the quicktime jar
+    // if we can't it throws an extension exception ev 3/3/09
+    em.getFile("qtj/QTJava.jar");
+    em.addToLibraryPath(this, "lib");
+  }
 
-    public void load( PrimitiveManager primitiveManager ) 
-	{
-		primitiveManager.addPrimitive
-			( "camera-image" , new Capture.Image() ) ;
-		primitiveManager.addPrimitive
-			( "camera-start" , new Capture.StartCamera() ) ;
-		primitiveManager.addPrimitive
-			( "camera-stop" , new Capture.StopCamera() ) ;
-		primitiveManager.addPrimitive
-			( "camera-select" , new Capture.SelectCamera() ) ;
-		primitiveManager.addPrimitive
-			( "movie-open" , new Movie.OpenMovie() ) ;
-		primitiveManager.addPrimitive
-			( "movie-start" , new Movie.StartMovie() ) ;
-		primitiveManager.addPrimitive
-			( "movie-stop" , new Movie.StopMovie() ) ;
-		primitiveManager.addPrimitive
-			( "movie-open-player" , new Movie.OpenPlayer() ) ;
-		primitiveManager.addPrimitive
-			( "movie-close" , new Movie.CloseMovie() ) ;
-		primitiveManager.addPrimitive
-			( "movie-image" , new Movie.Image() ) ;
-		primitiveManager.addPrimitive
-			( "movie-set-time" , new Movie.SetTime() ) ;
+  public void load(PrimitiveManager primitiveManager) {
+    primitiveManager.addPrimitive
+        ("camera-image", new Capture.Image());
+    primitiveManager.addPrimitive
+        ("camera-start", new Capture.StartCamera());
+    primitiveManager.addPrimitive
+        ("camera-stop", new Capture.StopCamera());
+    primitiveManager.addPrimitive
+        ("camera-select", new Capture.SelectCamera());
+    primitiveManager.addPrimitive
+        ("movie-open", new Movie.OpenMovie());
+    primitiveManager.addPrimitive
+        ("movie-start", new Movie.StartMovie());
+    primitiveManager.addPrimitive
+        ("movie-stop", new Movie.StopMovie());
+    primitiveManager.addPrimitive
+        ("movie-open-player", new Movie.OpenPlayer());
+    primitiveManager.addPrimitive
+        ("movie-close", new Movie.CloseMovie());
+    primitiveManager.addPrimitive
+        ("movie-image", new Movie.Image());
+    primitiveManager.addPrimitive
+        ("movie-set-time", new Movie.SetTime());
+  }
+
+  public void unload(ExtensionManager em)
+      throws ExtensionException {
+    Movie.unload();
+    Capture.unload();
+    // Since native libraries cannot be loaded in more than one classloader at once
+    // and even though we are going dispose of this classloader we can't be sure
+    // it will be GC'd before we want to reload this extension, we unload it manually
+    // as described here: http://forums.sun.com/thread.jspa?forumID=52&threadID=283774
+    // This is a hack, but it works. ev 6/25/09
+    try {
+      ClassLoader classLoader = this.getClass().getClassLoader();
+      java.lang.reflect.Field field = ClassLoader.class.getDeclaredField("nativeLibraries");
+      field.setAccessible(true);
+      java.util.Vector libs = (java.util.Vector) field.get(classLoader);
+      for (Object o : libs) {
+        java.lang.reflect.Method finalize = o.getClass().getDeclaredMethod("finalize", new Class[0]);
+        finalize.setAccessible(true);
+        finalize.invoke(o, new Object[0]);
+      }
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
     }
+  }
 
-	public void unload( ExtensionManager em )
-		throws ExtensionException
-	{
-		Movie.unload() ;
-		Capture.unload() ;
-		// Since native libraries cannot be loaded in more than one classloader at once
-		// and even though we are going dispose of this classloader we can't be sure
-		// it will be GC'd before we want to reload this extension, we unload it manually
-		// as described here: http://forums.sun.com/thread.jspa?forumID=52&threadID=283774
-		// This is a hack, but it works. ev 6/25/09
-		try
-		{
-			ClassLoader classLoader = this.getClass().getClassLoader() ;
-			java.lang.reflect.Field field = ClassLoader.class.getDeclaredField( "nativeLibraries" ) ;
-			field.setAccessible(true);
-			java.util.Vector libs = (java.util.Vector) field.get(classLoader) ;
-			for ( Object o : libs ) 
-			{
-				java.lang.reflect.Method finalize = o.getClass().getDeclaredMethod( "finalize" , new Class[0] ) ;
-				finalize.setAccessible( true ) ;
-				finalize.invoke( o , new Object[0] ) ;
-			}
-		}
-		catch( Exception e )
-		{
-			System.err.println( e.getMessage() ) ;
-		}
-	}
+  static java.awt.image.BufferedImage getBufferedImage(int[] data, int width, int height) {
+    return new java.awt.image.BufferedImage
+        (colorModel, getRaster(getRGBSampleModel(width, height), data), false, null);
+  }
 
-	static java.awt.image.BufferedImage getBufferedImage( int [] data , int width , int height )
-	{
-		return new java.awt.image.BufferedImage
-			( colorModel , getRaster( getRGBSampleModel( width , height ) , data ) , false , null ) ;
-	}
+  private static final DirectColorModel colorModel = new DirectColorModel(32, 0xff0000, 0xff00, 0xff);
 
-	private static final DirectColorModel colorModel = new DirectColorModel( 32 , 0xff0000 , 0xff00 , 0xff ) ;
+  private static SampleModel getRGBSampleModel(int width, int height) {
+    WritableRaster wr = colorModel.createCompatibleWritableRaster(1, 1);
+    SampleModel sampleModel = wr.getSampleModel();
+    sampleModel = sampleModel.createCompatibleSampleModel(width, height);
+    return sampleModel;
+  }
 
-	private static SampleModel getRGBSampleModel( int width , int height )
-	{
-		WritableRaster wr = colorModel.createCompatibleWritableRaster( 1 , 1 ) ;
-		SampleModel sampleModel = wr.getSampleModel() ;
-		sampleModel = sampleModel.createCompatibleSampleModel( width , height ) ;
-		return sampleModel ;
-	}
+  private static WritableRaster getRaster(SampleModel model, int[] data) {
+    return Raster.createWritableRaster
+        (model, new DataBufferInt(data, data.length, 0), null);
+  }
 
-	private static WritableRaster getRaster( SampleModel model , int [] data )
-	{
-		return Raster.createWritableRaster
-			( model , new DataBufferInt( data , data.length , 0 ) , null ) ;
-	}
-
-    @Override
-    public java.util.List<String> additionalJars() {
-        return new java.util.ArrayList<String>() {{
-            add("QTJava.jar");
-        }};
-    }
+  @Override
+  public java.util.List<String> additionalJars() {
+    return new java.util.ArrayList<String>() {{
+      add("QTJava.jar");
+    }};
+  }
 }
